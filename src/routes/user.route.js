86096a6d86224883bf40e295c2905ba4a3cf05df
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-10-11 20:49:09 -0300

    feat: route to find all the users on db

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -2,5 +2,7 @@ const route = require("express").Router();
 const userController = require("../controllers/user.controller");
 
 route.post("/", userController.create);
+route.get("/", userController.findAll);
+
 
 module.exports = route;
