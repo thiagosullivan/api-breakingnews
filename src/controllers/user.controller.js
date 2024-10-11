aee7c215905a9b96981883510ca5523316d0a4fe
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-10-11 20:49:50 -0300

    feat: controller to finde all the users on db

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,5 @@
 const userService = require("../services/user.service");
+const mongoose = require("mongoose");
 
 const create = async (req, res) => {
   const { name, username, email, password, avatar, background } = req.body;
@@ -7,7 +8,7 @@ const create = async (req, res) => {
     res.status(400).send({ message: "Submit all fields for registration" });
   }
 
-  const user = await userService.create(req.body);
+  const user = await userService.createService(req.body);
 
   if (!user) {
     res.status(400).send({ message: "Error creating user!" });
@@ -26,4 +27,15 @@ const create = async (req, res) => {
   });
 };
 
-module.exports = { create };
+const findAll = async (req, res) => {
+  const users = await userService.findAllService();
+
+  if (users.length === 0) {
+    return res.status(400).send({ message: "There are no registered users" });
+  }
+
+  res.send(users);
+};
+
+
+module.exports = { create, findAll };
