f271684555784a457c844d1165bd99447064a295
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-10-13 15:03:26 -0300

    feat: function to update of the users on db

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const create = async (req, res) => {
   const { name, username, email, password, avatar, background } = req.body;
 
-  if ((!name, !username, !email, !password, !avatar, !background)) {
+  if (!name || !username || !email || !password || !avatar || !background) {
     res.status(400).send({ message: "Submit all fields for registration" });
   }
 
@@ -40,7 +40,7 @@ const findAll = async (req, res) => {
 const findById = async (req, res) => {
   const id = req.params.id;
 
-  if(!mongoose.Types.ObjectId.isValid(id)){
+  if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(400).send({ message: "Invalid ID" });
   }
 
@@ -53,4 +53,36 @@ const findById = async (req, res) => {
   res.send(user);
 };
 
-module.exports = { create, findAll, findById };
+const update = async (req, res) => {
+  const { name, username, email, password, avatar, background } = req.body;
+
+  if (!name && !username && !email && !password && !avatar && !background) {
+    res.status(400).send({ message: "Submit at least one field for update" });
+  }
+
+  const id = req.params.id;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).send({ message: "Invalid ID" });
+  }
+
+  const user = await userService.findByIdService(id);
+
+  if (!user) {
+    return res.status(400).send({ message: "User not found" });
+  }
+
+  await userService.updateService(
+    id,
+    name,
+    username,
+    email,
+    password,
+    avatar,
+    background
+  );
+
+  res.send({ message: "User successfully updated!" });
+};
+
+module.exports = { create, findAll, findById, update };
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,13 +1,36 @@
 const User = require("../models/User");
 
-const createService = (body) => User.create(body)
+const createService = (body) => User.create(body);
 
 const findAllService = () => User.find();
 
 const findByIdService = (id) => User.findById(id);
 
+const updateService = (
+  id,
+  name,
+  username,
+  email,
+  password,
+  avatar,
+  background
+) => User.findOneAndUpdate(
+    {
+        _id: id
+    },
+    {
+      name,
+      username,
+      email,
+      password,
+      avatar,
+      background,
+    }
+  );
+
 module.exports = {
-    createService,
-    findAllService,
-    findByIdService,
-}
+  createService,
+  findAllService,
+  findByIdService,
+  updateService
+};
