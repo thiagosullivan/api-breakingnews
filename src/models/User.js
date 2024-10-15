a828a5f36523a49152543fa3bbf802cb3c0d1b71
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-10-15 01:26:23 -0300

    feat: encrypting password before send to db

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -17,6 +18,7 @@ const UserSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true,
+        select: false,
     },
     avatar: {
         type: String,
@@ -26,6 +28,11 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true,
     }
+})
+
+UserSchema.pre("save", async function(next){
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
 })
 
 const User = mongoose.model("User", UserSchema);
