d6f8de4dda7208de6b1c8d7c532170ca4f02df63
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-10-16 00:28:40 -0300

    feat: added controller e service for create News

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,34 +1,36 @@
-import {createService, findAllService} from '../services/news.service.js';
+import { createService, findAllService } from "../services/news.service.js";
 
 const create = async (req, res) => {
-    try {
-        const {title, text, banner} = req.body;
+  try {
+    const { title, text, banner } = req.body;
 
-        if(!title || !text || !banner){
-            res.status(400).send({
-                message: "Submit all fields for registration",
-            })
-        }
-
-        await createService({
-            title,
-            text,
-            banner,
-            id: "objectIdFake1"
-        })
-
-        res.send(201)
-    } catch (error) {
-        res.status(500).send({ message: error.message})
+    if (!title || !text || !banner) {
+      res.status(400).send({
+        message: "Submit all fields for registration",
+      });
     }
-}
 
-const findAll = (req, res) => {
-    const news = []
-    res.send(news)
-}
+    await createService({
+      title,
+      text,
+      banner,
+      user: { _id: "670dee40b44570fe1861fb56" },
+    });
 
-export default {
-    create,
-    findAll
-}
+    res.send(201);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+};
+
+const findAll = async (req, res) => {
+  const news = await findAllService();
+
+  if (news.length === 0) {
+    return res.status(400).send({ message: "There are no registered news" });
+  }
+
+  res.send(news);
+};
+
+export { create, findAll };
