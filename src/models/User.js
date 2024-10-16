a2017370ebc5d048cacee30a5164ae2ff5a9923e
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-10-16 00:07:43 -0300

    refact: adding new field to User Schema

--- src/models/User.js
+++ src/models/User.js
@@ -9,11 +9,13 @@ const UserSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
+        unique: true,
     },
     email: {
         type: String,
         required: true,
         unique: true,
+        lowercase: true,
     },
     password: {
         type: String,
