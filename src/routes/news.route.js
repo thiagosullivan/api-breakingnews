debf1aae74f92f79f06dc4c44b11539be699e5ca
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-10-18 11:39:48 -0300

    feat: add Top News section

--- src/routes/news.route.js
+++ src/routes/news.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { create, findAll } from "../controllers/news.controller.js";
+import { create, findAll, topNews } from "../controllers/news.controller.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.post("/", authMiddleware, create);
 router.get("/", findAll);
+router.get("/top", topNews);
 
 export default router;
